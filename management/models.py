8bdacd7e16d98ef36d5c11a9e40dbb0d7c1aa1bc
alevcoder <alevcoder1@gmail.com>
2021-11-26 12:13:51 +0500

    mehnat public

---
+++ management/models.py
@@ -0,0 +1,78 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Managment(models.Model):
+    name = models.CharField(verbose_name="F.I.SH", max_length=250)
+    title = models.CharField(verbose_name="Lavozimi", max_length=250)
+    phone = models.CharField(verbose_name="Phone", max_length=15)
+    acceptance_dates = models.CharField(verbose_name="Qabul kunlari", max_length=250)
+    biography = models.TextField(verbose_name="Tarjimai hol", null=True, blank=True)
+    functions_tasks = models.TextField(verbose_name="Funksiya va vazifalar", null=True, blank=True)
+    date_time = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField()
+
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name="Rahbaryat"
+        verbose_name_plural="Rahbaryat azolari"
+    
+class Sections(models.Model):
+    name = models.CharField(verbose_name="F.I.SH", max_length=250)
+    title = models.CharField(verbose_name="Lavozimi", max_length=250)
+    phone = models.CharField(verbose_name="Phone", max_length=15)
+    acceptance_dates = models.CharField(verbose_name="Qabul kunlari", max_length=250)
+    biography = models.TextField(verbose_name="Tarjimai hol", null=True, blank=True)
+    functions_tasks = models.TextField(verbose_name="Funksiya va vazifalar", null=True, blank=True)
+    date_time = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField()
+
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name="Bo'lim"
+        verbose_name_plural="Bo'limlar"
+
+class RegionalCenters(models.Model):
+    name = models.CharField(verbose_name="Hududiy boshqarma nomi", max_length=250)
+    leader = models.CharField(verbose_name="F.I.SH BOshqarma boshlig'i", max_length=250)
+    title = models.CharField(verbose_name="Manzili", max_length=250)
+    phone = models.CharField(verbose_name="Phone", max_length=15)
+    date_time = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+            return self.name
+    class Meta:
+        verbose_name="Hududiy bo'lim"
+        verbose_name_plural="Hududiy bo'limlar"
+        
+class Post(models.Model):
+    user = models.ForeignKey(User,verbose_name='Muallif', on_delete=models.CASCADE)
+    title = models.CharField(verbose_name="Qisqa matn", max_length=300)
+    Text = models.TextField(verbose_name="Matin", null=True, blank=True)
+    image = models.ImageField()
+    date  = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name="Yangilik"
+        verbose_name_plural="Yangiliklar"
+
+
+# Create your models here.
+class LiveMessage(models.Model):
+    name = models.CharField(verbose_name="Name", max_length=100)
+    phone = models.CharField(verbose_name="Phone", max_length=100)
+    message = models.CharField(verbose_name="Message", max_length=100)
+    message_long = models.TextField(verbose_name="Message long")
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = 'Xabar'
+        verbose_name_plural = 'Xabarlar'
---
+++ management/views.py
@@ -0,0 +1,118 @@
+from django.db import models
+from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView,View
+from .models import Managment, Sections, RegionalCenters, Post
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from .helpers import create_ad
+from django.contrib import messages
+from django.db.models import Q
+
+# Create your views here.
+class GlobaslView(ListView):
+    model = Post
+    template_name = 'index.html'
+    context_object_name = 'news'
+
+# class Category(ListView):
+class ManagmentView(TemplateView):
+    template_name = 'managment.html'
+    model = Managment
+    def get_context_data(self, **kwargs):
+        context = super(ManagmentView, self).get_context_data(**kwargs)
+        context['managers_list'] = Managment.objects.all().reverse()
+        return context
+
+
+class SectionsView(TemplateView):
+    template_name = 'sections.html'
+    model = Sections
+    def get_context_data(self, **kwargs):
+        context = super(SectionsView, self).get_context_data(**kwargs)
+        context['sections_list'] = Sections.objects.all().reverse()
+        return context
+
+
+class RegionalView(TemplateView):
+    template_name = 'regional.html'
+    model = RegionalCenters
+    def get_context_data(self, **kwargs):
+        context = super(RegionalView, self).get_context_data(**kwargs)
+        context['regions_list'] = RegionalCenters.objects.all().reverse()
+        return context
+
+# New View
+class NewsView(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news_list'
+    paginate_by = 3
+    
+    # def get_context_data(self, **kwargs):
+    #     context = super(NewsView, self).get_context_data(**kwargs)
+    #     try:
+    #          users = paginator.page(page)
+    #     except PageNotAnInteger:
+    #         users = paginator.page(1)
+    #     except EmptyPage:
+    #         users = paginator.page(paginator.num_pages)
+    #     context['news_list'] = Post.objects.all()
+
+    #     return context
+#End New View
+
+
+
+# New Ditail View
+class NewsDetailView(DetailView):
+    model = Post
+    template_name = 'detail_news.html'
+    context_object_name = 'news_detail'
+    # def get_context_data(self, **kwargs):
+    #     context = super(NewsDetailView,self).get_context_data(**kwargs)
+    #     detail_post = Post.objects.all()
+        
+    #     context['news_detail'] = detail_post
+        # return context
+
+# End New Ditail View
+
+
+#Form action  
+class ActinView(View):
+    
+    def post(self, request):
+        post_request=request.POST
+        actions = {
+            'create_ad':create_ad,
+        }
+        messages.success(request, 'Xabaringiz yuborildi')
+        actions[self.request.POST.get('action', None)](post_request)
+        return redirect('/')
+# end Form action
+
+
+# Gerb Madhiya Bayroq 
+
+class GerbView(TemplateView):
+    template_name = 'gerb.html'
+
+class BayroqView(TemplateView):
+    template_name = 'bayroq.html'
+
+class MadhiyaView(TemplateView):
+    template_name = 'madhiya.html'
+
+# Gerb Madhiya Bayroq  end
+
+
+# Search View
+class SearchResulView(ListView):
+    model = Post
+    template_name = 'search.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        objecy_list = Post.objects.filter(Q(title__icontains=query)|Q(Text__icontains=query))
+        return objecy_list
